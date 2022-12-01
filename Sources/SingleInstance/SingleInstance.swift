59f474f9c2112acdd08055cbd729dc49af7d1403
mm <mmdevstuff@gmail.com>
2022-12-01 15:28:22 -0800

    Revert "update"
    This reverts commit 5f5fb4d909fba3003d07442e31ba894f8074d3d0.

--- Sources/SingleInstance/SingleInstance.swift
+++ Sources/SingleInstance/SingleInstance.swift
@@ -1,22 +1,9 @@
 // SingleInstance.swift
 import Foundation
 
-/// SingleInstances differ from the Singleton `.shared()` pattern by
-/// requiring failable initialzation. If initialization fails, there is an extant
-/// single instance somewhere.
-///
-/// Guidelines:
-///
-/// - Use Singleton pattern to share a Utility object
-/// - Use SingleInstance pattern for non-shared objects (esp. dependency injection)
-/// - Parent object should have at least same lifespan as SingleInstance and hold strong reference throughout. References at injection site should be `unowned` to call out that these values are injected.
-///
-/// Unlike Singletons which are lazily initialized and shared, the lifecycle of SingleInstances
-/// are explicit in the code and few lines of code should have access to the object, reducing
-/// potential down-the-line technical debt when original coders are replaced, and also providing
-/// a clean state for each test case, since the Singleton's typically do not deinitialize.
-
-open class SingleInstance {
+/// The only reason to split root of `SingleInstance` out is so subclasses will not have to use
+/// the `override` keyword when declaring their failable initializer.
+open class SingleInstanceRoot {
     fileprivate struct WeakValue {
         weak var value: AnyObject?
     }
@@ -32,13 +19,33 @@ open class SingleInstance {
     }
 
     fileprivate init?() {
-        guard SingleInstance.instance[key] == nil else { return nil }
+        guard SingleInstanceRoot.instance[key] == nil else { return nil }
         if !exemptedFromSingleInstance() {
-            SingleInstance.instance[key] = WeakValue(value: self)
+            SingleInstanceRoot.instance[key] = WeakValue(value: self)
         }
     }
 
     deinit {
-        SingleInstance.instance[key] = nil
+        SingleInstanceRoot.instance[key] = nil
+    }
+}
+
+/// SingleInstances differ from the Singleton `.shared()` pattern by
+/// requiring failable initialzation. If initialization fails, there is an extant
+/// single instance somewhere.
+///
+/// Guidelines:
+///
+/// - Use Singleton pattern to share a Utility object
+/// - Use SingleInstance pattern for non-shared objects (esp. dependency injection)
+/// - Parent object should have at least same lifespan as SingleInstance and hold strong reference throughout. References at injection site should be `unowned` to call out that these values are injected.
+///
+/// Unlike Singletons which are lazily initialized and shared, the lifecycle of SingleInstances
+/// are explicit in the code and few lines of code should have access to the object, reducing
+/// potential down-the-line technical debt when original coders are replaced, and also providing
+/// a clean state for each test case, since the Singleton's typically do not deinitialize.
+open class SingleInstance: SingleInstanceRoot {
+    public override required init?() {
+        super.init()
     }
 }
